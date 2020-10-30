bf84104dca7dc5adcb763d6ec9065b5131ff09bf
billyott <willrichott@gmail.com>
2020-10-30 10:32:22 -0400

    Render cards on page

--- src/index.js
+++ src/index.js
@@ -13,3 +13,52 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+//**************** define variables ****************
+
+//for targeting html elements
+const toyCollectionDiv = document.querySelector("#toy-collection")
+
+//**************** define functions ****************
+
+//html render
+function renderToy(toy) {
+  const toyCard = document.createElement("div")
+  toyCard.className = "card"
+  toyCard.dataset.id = toy.id
+  toyCard.innerHTML = `
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p>${toy.likes} Likes </p>
+    <button class="like-btn">Like <3</button>`
+  toyCollectionDiv.append(toyCard)
+}
+
+function renderAllToys(toyArray) {
+  toyArray.forEach(renderToy)
+}
+
+//fetch requests
+function getAllToys() {
+  return fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+}
+
+//event listeners
+
+//event handlers
+
+//initialization
+function initialize() {
+  getAllToys()
+    .then(toyArray => {
+      renderAllToys(toyArray)
+    })
+    .catch(errors => {
+      alert("Failed to grab all toys :(")
+    })
+}
+
+//**************** invoke functions ****************
+initialize()
